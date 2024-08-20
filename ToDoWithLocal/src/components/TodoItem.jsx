fb2ff5c7fd625466b11ae15f6e45ceb66069c381
muhammadyaqoobmuet <yaqoobahmed45700@gmai.com>
2024-08-20 12:47:16 +0500

    understand local storage

--- ToDoWithLocal/src/components/TodoItem.jsx
+++ ToDoWithLocal/src/components/TodoItem.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 function TodoItem({ todo }) {
     const { updateToDo, deleteToDo, toggleComplete } = useToDo();
-    const [isToDoEditable, setIsToDoEditable] = useState(false);
+    const [isToDoEditable, setIsToDoEditable] = useState(false); // just for icons to change their state
     const [todoMsg, setTodoMsg] = useState(todo.todo);
 
     const editTodo = () => {
@@ -17,7 +17,8 @@ function TodoItem({ todo }) {
 
     return (
         <div
-            className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300 text-black ${
+            className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3
+             shadow-sm shadow-white/50 duration-300 text-black ${
                 todo.completed ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"
             }`}
         >
@@ -37,13 +38,15 @@ function TodoItem({ todo }) {
                 readOnly={!isToDoEditable}
             />
             <button
-                className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50"
+                className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 
+                            justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50"
                 onClick={() => {
                     if (todo.completed) return;
 
                     if (isToDoEditable) {
                         editTodo();
-                    } else setIsToDoEditable((prev) => !prev);
+                    } 
+                    else setIsToDoEditable((prev) => !prev);
                 }}
                 disabled={todo.completed}
             >
