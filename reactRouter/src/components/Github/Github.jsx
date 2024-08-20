6c5075267b048dd5927778d4d4bf236bdf8c102c
muhammadyaqoobmuet <yaqoobahmed45700@gmai.com>
2024-08-20 12:45:35 +0500

    react router crash course

--- reactRouter/src/components/Github/Github.jsx
+++ reactRouter/src/components/Github/Github.jsx
@@ -1,19 +1,22 @@
 import React, { useEffect, useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 function Github() {
-    const [data, setData] = useState(null);
+const data = useLoaderData();
 
-    useEffect(() => {
-        fetch('https://api.github.com/users/muhammadyaqoobmuet')
-            .then(resp => resp.json())
-            .then(resp => {
-                setData(resp);
-            })
-            .catch(error => console.error('Error fetching data:', error));
-    }, []); // Added empty dependency array to ensure it only runs once
+    // const [data, setData] = useState(null);
+
+    // useEffect(() => {
+    //     fetch('')
+    //         .then(resp => resp.json())
+    //         .then(resp => {
+    //             setData(resp);
+    //         })
+    //         .catch(error => console.error('Error fetching data:', error));
+    // }, []); // Added empty dependency array to ensure it only runs once
 
     if (!data) {
-        return <div>Loading...</div>; // Display loading message while fetching data
+        return <div className='text-center mt-20 my-20 py-11 font-bold text-5xl text-sky-500'>Loading...</div>; // Display loading message while fetching data
     }
 
     return (
@@ -22,17 +25,17 @@ function Github() {
                 <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
                     <div className="md:5/12 lg:w-5/12">
                         <img
-                            src=""
+                            src={data.avatar_url}
                             alt="User Profile"
                             className="rounded-full w-48 h-48 object-cover mx-auto md:mx-0"
                         />
                     </div>
                     <div className="md:7/12 lg:w-6/12">
                         <h2 className="text-3xl text-gray-900 font-bold md:text-4xl">
-                           name
+                           {data.name}
                         </h2>
                         <p className="mt-6 text-gray-600">
-                            data
+                        <span className='text-xl font-bold '>Company : </span> {data.company || "no company yet"}
                         </p>
                         <p className="mt-4 text-gray-600">
                             Lover of clean code, user-centered design, and continuous learning. Always eager to take on new challenges and collaborate with like-minded individuals.
@@ -52,4 +55,8 @@ function Github() {
     );
 }
 
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/muhammadyaqoobmuet')
+    return response.json()
+}
 export default Github;
--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -1,54 +1,73 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Route } from 'react-router-dom'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createRoutesFromElements, Route } from "react-router-dom";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import Home from './components/Home/Home'
-import About from './components/About/About'
-import Layout from './Layout'
-import Contact from './components/Contact/Contact'
-import User from './components/User/User'
-import Github from './components/Github/Github'
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Layout from "./Layout";
+import Contact from "./components/Contact/Contact";
+import User from "./components/User/User";
+import Github, { githubInfoLoader } from "./components/Github/Github";
 
+// const router = createBrowserRouter([
+//   {
+//     path:'/',
+//     element:<Layout/>,
+//     children: [
+//       {
+//         path:"",
+//         element:<Home/>,
+//       },
+//       {
+//         path:"about",
+//         element:<About/>,
+//       },
+//       {
+//         path:"contact-us",
+//         element:<Contact/>
+//       },
+//         {
+//           path:"user/:userid",
+//           element:<User/>
+//         },
 
-const router = createBrowserRouter([
-  {
-    path:'/',
-    element:<Layout/>,
-    children: [
-      {
-        path:"",
-        element:<Home/>,
-      },
-      {
-        path:"about",
-        element:<About/>,
-      },
-      {
-        path:"contact-us",
-        element:<Contact/>  
-      },
-        {
-          path:"user/:userid",
-          element:<User/>  
-        },
-        
-       {
-            path:"github",
-            element:<Github/>  
-          }
-        
-      
-    ]
-  }
-])
+//        {
+//             path:"github",
+//             element:<Github/>
+//           }
 
+//     ]
+//   }
+// ])
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-   
-  <>
-    <RouterProvider  router={router}/>
-  </>
-  
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+
+//   <>
+//     <RouterProvider  router={router}/>
+//   </>
+
+// )
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact-us" element={<Contact />} />
+      <Route path="user/:userid" element={<User />} />
+      <Route
+      loader={githubInfoLoader}
+       path="github"
+        element={<Github />}
+         />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
